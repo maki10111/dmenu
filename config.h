e3e91121669587f5cf600ce5e22eb6c4782fe863
maki10111 <makibyet101@gmail.com>
2020-11-02 16:15:33 +0000

    transparent

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const char *fonts[] = {
 	"MesloLGS NF:size=8",
 	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
-static const unsigned int bgalpha = 0xe0;
+static const unsigned int bgalpha = 0xa0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
