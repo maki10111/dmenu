05035b3a093d0e42eb1ac12fd9523432abe07b31
maki101111 <makibyet101@gmail.com>
2021-05-22 21:44:43 +0800

    changes

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"MesloLGS NF:size=8",
+	"Source Code Pro:size=10",
 	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xa0;
